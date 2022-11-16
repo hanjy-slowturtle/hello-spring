3bbf08675c9e0049b709f8bfc521bdf184d1bbc4
hanjy-slowturtle <h920915@gmail.com>
2022-11-16 23:16:46 +0900

    ~ MVC와 템플릿 엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,5 +15,11 @@ public class HelloController {
 		model.addAttribute("data", "hello!!");
 		model.addAttribute("random", new Random().nextInt());
 		return "hello";
+	}
+	
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello-template";
 	}
 }
