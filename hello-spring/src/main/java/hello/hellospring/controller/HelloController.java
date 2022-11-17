48f2eaf3cb421a808f5770634bf46b8325581ba9
hanjy-slowturtle <h920915@gmail.com>
2022-11-17 23:24:14 +0900

    ~ API

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,11 +1,14 @@
 package hello.hellospring.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,5 +24,40 @@ public class HelloController {
 	public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello-template";
+	}
+	
+	@GetMapping("hello-string")
+	@ResponseBody
+	public String helloString(@RequestParam("name") String name) {
+		return "hello " + name;
+	}
+	
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Hello helloApi(@RequestParam("name") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		return hello;
+	}
+	
+	@GetMapping("hello-api2")
+	@ResponseBody
+	public Map<String, Object> helloApi2(@RequestParam(value = "name", required = false) String name) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", name);
+		map.put("test", null);
+		return map;
+	}
+	
+	static class Hello {
+		private String name;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 }
